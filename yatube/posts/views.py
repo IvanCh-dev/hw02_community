75a23feaab9a268735dd9de3b91b09fcbd74feca
Ivan Chuzhba <ivan94-20@yandex.ru>
2022-07-30 23:29:43 +0600

    Fixed

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post, Group
 
 
+DISPLAYED_POSTS = 10
+
+
 def index(request):
-    displayed_posts = 10
     title = 'Последние обновления на сайте'
-    posts = Post.objects.select_related('group')[:displayed_posts]
+    posts = Post.objects.select_related('group')[:DISPLAYED_POSTS]
     context = {
         'title': title,
         'posts': posts,
@@ -14,9 +16,8 @@ def index(request):
 
 
 def group_posts(request, slug):
-    displayed_posts = 10
     group = get_object_or_404(Group, slug=slug)
-    posts = group.posts.all()[:displayed_posts]
+    posts = group.posts.all()[:DISPLAYED_POSTS]
     context = {
         'group': group,
         'posts': posts,
